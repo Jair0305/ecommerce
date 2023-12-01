67cff61c5cdea09a52dace832a8b6be20b0016db
Jair0305 <chamaco_03@hotmail.es>
2023-12-01 02:44:54 -0600

    added a nabar and a carousel

---
+++ frontend/src/customer/components/HomeSectionCard/HomeSectionCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const HomeSectionCard = () => {
+  return (
+    <div className='cursor-pointer flex flex-col items-center bg-white rounded-lg shadow-ls overflow-hidden w-[15rem] mx-3'>
+        <div className='h-[13rem] w-[10rem]'>
+            <img className='object-cover object-top w-full h-full' src="https://rukminim2.flixcart.com/image/612/612/xif0q/shirt/t/l/x/40-pcsfsslpm20035-peter-england-original-imagt4sy8azcgyxe.jpeg?q=70"></img>
+
+        </div>
+        <div className='p-4'>
+            <h3></h3>
+        </div>
+    </div>
+  )
+}
+
+export default HomeSectionCard
