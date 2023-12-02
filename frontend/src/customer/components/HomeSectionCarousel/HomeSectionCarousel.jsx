90191ff40ff02b8bb0ae71ec21c3e92d37527e68
Jair0305 <chamaco_03@hotmail.es>
2023-12-02 14:39:20 -0600

    fixed the carousel function and added a footer

--- frontend/src/customer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
+++ frontend/src/customer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
@@ -1,38 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 import HomeSectionCard from "../HomeSectionCard/HomeSectionCard";
 import AliceCarousel from "react-alice-carousel";
 import { Button } from "@mui/material";
 import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import { mens_kurta } from "../../../Data/mens_kurta";
 
-const HomeSectionCarousel = () => {
+const HomeSectionCarousel = ({data, sectionName}) => {
+    const[activeIndex, setActiveIndex] = useState(0);
 
     const responsive = {
         0: {items: 1},
         720: {items: 3},
         1024: {items: 5.5},
-    }
+    };
 
-    const items =[1,1,1,1,1].map((item)=> <HomeSectionCard/>)
+    const slidePrev=()=>setActiveIndex(activeIndex-1);
+    const slideNext=()=>setActiveIndex(activeIndex+1);
+
+    const syncActiveIndex = ({item})=>setActiveIndex(item);
+
+    const items = data.slice(0,10).map((item)=> <HomeSectionCard product={item}/>);
     
     return(
-        <div className=" px-4 lg:pg-8 border border-black">
+        <div className=" border ">
+            <h2 className="text-2x2 font-extrabold text-gray-800 py-5">{sectionName}</h2>
             <div className="relative p-5">
                 <AliceCarousel
                     items={items}
                     disableButtonsControls
-                    infinite
                     responsive={responsive}
                     disableDotsControls
+                    onSlideChanged={syncActiveIndex}
+                    activeIndex={activeIndex}
                 />
-                <Button variant="contained" className="z-50 bg-white" sx={{position:'absolute', top:"8rem", right:"0rem", transform:"translateX(50%) rotate(90deg)", bgcolor:"white",}} aria-label="next">
-                    <KeyboardArrowLeftIcon sx={{transform:"rotate(90deg)", color:"black"}}/>
-                </Button>
-                <Button variant="contained" className="z-50 bg-white" sx={{position:'absolute', top:"8rem", left:"0rem", transform:"translateX(50%) rotate(-90deg)", bgcolor:"white",}} aria-label="next">
-                    <KeyboardArrowLeftIcon sx={{transform:"rotate(90deg)", color:"black"}}/>
-                </Button>
+                {activeIndex!==items.length-5 && <Button 
+                    variant="contained" 
+                    className="z-50 bg-white"
+                    onClick={slideNext}
+                    sx={{
+                        position:'absolute', 
+                        top:"8rem", 
+                        right:"0rem", 
+                        transform:"translateX(50%) rotate(90deg)", 
+                        bgcolor:"white",
+                    }} 
+                    aria-label="next">
+                    <KeyboardArrowLeftIcon 
+                        sx={{
+                            transform:"rotate(90deg)", 
+                            color:"black"
+                        }}
+                    />
+                </Button>}
+                {activeIndex !==0 && <Button 
+                    onClick={slidePrev}
+                    variant="contained" 
+                    className="z-50 bg-white" 
+                    sx={{
+                        position:'absolute', 
+                        top:"8rem", 
+                        left:"0rem", 
+                        transform:"translateX(-50%) rotate(-90deg)", 
+                        bgcolor:"white",
+                    }} 
+                    aria-label="next">
+                    <KeyboardArrowLeftIcon 
+                        sx={{
+                            transform:"rotate(90deg)", 
+                            color:"black"
+                        }}
+                    />
+                </Button>}
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default HomeSectionCarousel
